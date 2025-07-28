afbaae22fe575ae01262f6b0eb93219e79d64593
theboy0112 <clifboycabrera12@gmail.com>
2025-07-28 15:45:09 +0800

    fixqueue

--- src/queue/queue.resolver.ts
+++ src/queue/queue.resolver.ts
@@ -3,7 +3,6 @@ import { QueueService } from './queue.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Queue } from 'src/queue/entities/queue.model';
 
-
 @Resolver(() => Queue)
 export class QueueResolver {
   constructor(
@@ -18,13 +17,25 @@ export class QueueResolver {
     return this.prisma.queue.findMany({
       where: { departmentId },
       orderBy: { createdAt: 'asc' },
-    }); 
+    });
   }
 
   @Mutation(() => String)
   async createQueue(
     @Args('departmentId', { type: () => Int }) departmentId: number,
   ): Promise<string> {
+    const department = await this.prisma.department.findUnique({
+      where: { id: departmentId },
+    });
+
+    if (!department) {
+      throw new Error('Department does not exist');
+    }
+
+    if (department.departmentName.toUpperCase() === 'ADMIN') {
+      throw new Error('ADMIN is not allowed to create a queue');
+    }
+
     const count = await this.prisma.queue.count({
       where: { departmentId },
     });
