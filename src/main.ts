abd0651965aa3fd97d5f6613d0bfee5bb33db5fb
theboy0112 <clifboycabrera12@gmail.com>
2025-07-31 16:57:59 +0800

    addedPort

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const port = 3000;
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://1rzx9lxq-5173.asse.devtunnels.ms/form'
+    ],
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? port);
   console.log('App running at ' ,port)
